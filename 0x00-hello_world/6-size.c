638f0ff62d8cafcda0f152555491936a63ce9e23
IamKaki <rizkarize@gmail.com>
2023-10-05 19:41:36 -0700

    Show sizes of data types on machine

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program to determine size of int, float, double and char
+ * Return:- this is to return 0
+ */
+
+int main(void)
+{
+	int integerType;
+	char charType;
+	float floatType;
+	double doubleType;
+
+	printf("The size of int: %ld\n", sizeof(intergerType));
+	printf("The size of char: %ld\n", sizeof(charType));
+	printf("The size of float: %ld\n", sizeof(floatType));
+	printf("The size of double: %ld\n", sizeof(doubleType));
+
+	return (0);
+}
