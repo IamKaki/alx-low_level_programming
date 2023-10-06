476b40174774a01c85b5b9bca609ee119e682af9
IamKaki <rizkarize@gmail.com>
2023-10-05 19:06:50 -0700

    This is puts at work

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is to test puts function in C
+ * Return: - this is to return 0
+ */
+
+int main(void)
+{
+	char string[60] = "\"Programming is like building a multilingual puzzle";
+	
+	puts(string);
+
+	return (0);
+}
