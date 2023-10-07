0565a5eae4d2d4b75e3148389c0eaff8504bb7db
IamKaki <rizkarize@gmail.com>
2023-10-06 18:30:27 -0700

    This is my first else-if ladder programm

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
+/*
+ * main - this is a programm to test else is ladder
+ * Return: - this is to return a 0
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
