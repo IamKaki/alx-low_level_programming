9b5f3f6e802f6a687fb572917abe90b39a6c4327
IamKaki <rizkarize@gmail.com>
2023-10-08 13:48:49 -0700

    printing out base 10 digits plus new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program to print base 10 numbers
+ * Return: - Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+	return (0);
+}
