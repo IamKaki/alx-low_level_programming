c85ac3b83f95cd91ed62655225fe84778d3bcf07
IamKaki <rizkarize@gmail.com>
2023-10-08 14:51:36 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print using putchar but no variable of type char
+ * Return: - Always 0
+ */
+
+int main(void)
+{
+	int var;
+	for (var = 0; var <= 9; var++)
+		putchar (var + 48);
+	putchar ('\n');
+
+	return (0);
+}
